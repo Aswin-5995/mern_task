891af78f6d5a443226dcfc3f4f53e2a8aa7f4137
Aswin R <cosai.ashwin@gmail.com>
2025-12-15 17:42:20 +0530

    Delete all files

--- backend/middlewares/authentication.js
+++
@@ -1,7 +0,0 @@
-module.exports = (allowedRoles) => (req, res, next) => {
-  const role = req.headers.role;
-  if (!allowedRoles.includes(role)) {
-    return res.status(403).json({ message: "Access Denied" });
-  }
-  next();
-};
--- backend/middlewares/product.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = (req, res, next) => {
-  const { name, price, stock } = req.body;
-  if (!name || price <= 0 || stock < 0) {
-    return res.status(400).json({ message: "Invalid product data" });
-  }
-  next();
-};
